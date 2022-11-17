9e066d6dc2075a2ed7b67666e08b8a75673bb960
parham zare <parhamzare@parhams-MacBook-Air.local>
2022-11-17 13:34:51 +0330

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import GuessResult from "./GuessResult";
-import { CountryResponseData, GenderResponseData } from "./types"
+import { AgeResponseData, CountryResponseData, GenderResponseData } from "./types";
 const App = () => {
+  // main code
   const [userInput, setUserInput] = useState<string>();
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<{ age: any, country: CountryResponseData, gender: GenderResponseData } | null>(null);
-  const getSearchResult = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLoading(true);
-    const endPoints = [
-      `https://api.agify.io/?name=${userInput}`,
-      `https://api.nationalize.io/?name=${userInput}`,
-      `https://api.genderize.io/?name=${userInput}`] as const
-    const [age, country, gender] = await Promise.all(endPoints.map(async (endPoint) => {
-      const response = await fetch(endPoint)
-      return response.json();
-    }))
-    setData({ age, country, gender });
-    setLoading(false);
-  }
+  const [usernameCheck, setUsernameCheck] = useState<string>("")
+  const [data, setData] = useState<{ age: AgeResponseData, country: CountryResponseData, gender: GenderResponseData }>();
+  const abortController = useRef<AbortController>(new AbortController())
+  const getSearchResult =
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      try {
+        if (userInput?.trim().toLowerCase() !== usernameCheck.trim().toLowerCase()) {
+          setUsernameCheck(userInput!);
+          setLoading(true);
+          if (loading === true) {
+            cancelRequest()
+          }
+          const endPoints = [
+            `https://api.agify.io/?name=${userInput?.trim().toLowerCase()}`,
+            `https://api.nationalize.io/?name=${userInput?.trim().toLowerCase()}`,
+            `https://api.genderize.io/?name=${userInput?.trim().toLowerCase()}`] as const
+          abortController.current = new AbortController();
+          const [age, country, gender] = await Promise.all(endPoints.map(async (endPoint) => {
+            const response = await fetch(endPoint, {
+              signal: abortController.current.signal
+            })
+            return response.json();
+          }))
+          setData({ age, country, gender });
+          setLoading(false);
+        }
+      }
+      catch (error) {
+        setUserInput("")
+      }
+    }
+  const cancelRequest = () => abortController.current && abortController.current.abort()
   return (
     <form className='guess' onSubmit={getSearchResult}>
       <div className='guess__search'>
